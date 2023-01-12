aeb40daa69fe3988002c3f16a1d9eb3e992bdcc4
BobMakhlin <borismakhlin5@gmail.com>
2023-01-12 20:18:38 +0200

    Refactoring

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,70 +4,88 @@ import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
-const emailReducer = (state, action) => {
-  switch (action.type) {
-    case "USER_INPUT":
-      return { value: action.value, isValid: null };
-    case "INPUT_BLUR":
-      return { value: state.value, isValid: state.value.includes("@") };
-    default:
-      return { value: "", isValid: false };
-  }
-};
+// on input we donot validate, we only update the value
+// on blur we donot reset the value, we validate
+// then the form validation is only checked on blur...
 
-const passwordReducer = (state, action) => {
+// const emailReducer = (state, action) => {
+//   switch (action.type) {
+//     case "USER_INPUT":
+//       return { value: action.value, isValid: null };
+//     case "INPUT_BLUR":
+//       return { value: state.value, isValid: state.value.includes("@") };
+//     default:
+//       return { value: "", isValid: false };
+//   }
+// };
+
+// const passwordReducer = (state, action) => {
+//   switch (action.type) {
+//     case "USER_INPUT":
+//       return { value: action.value, isValid: null };
+//     case "INPUT_BLUR":
+//       return { value: state.value, isValid: state.value.trim().length > 6 };
+//     default:
+//       return { value: "", isValid: null };
+//   }
+// };
+
+const formReducer = (state, action) => {
+  const stateCopy = { ...state };
+
   switch (action.type) {
-    case "USER_INPUT":
-      return { value: action.value, isValid: null };
-    case "INPUT_BLUR":
-      return { value: state.value, isValid: state.value.trim().length > 6 };
+    case "EMAIL_CHANGE":
+      stateCopy.email = action.value;
+      stateCopy.emailIsValid = null;
+      break;
+    case "PASSWORD_CHANGE":
+      stateCopy.password = action.value;
+      stateCopy.passwordIsValid = null;
+      break;
+    case "EMAIL_BLUR":
+      stateCopy.emailIsValid = state.email.includes("@");
+      break;
+    case "PASSWORD_BLUR":
+      stateCopy.passwordIsValid = state.password.trim().length > 6;
+      break;
     default:
-      return { value: "", isValid: null };
+      throw new Error(`Unsupported action: ${action.type}`);
   }
+
+  stateCopy.formIsValid =
+    stateCopy.emailIsValid === true && stateCopy.passwordIsValid === true;
+
+  return stateCopy;
 };
 
 const Login = (props) => {
-  const [formIsValid, setFormIsValid] = useState(false);
-
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: "",
-    isValid: null,
+  const [formState, dispatchForm] = useReducer(formReducer, {
+    email: "",
+    emailIsValid: null,
+    password: "",
+    passwordIsValid: null,
+    formIsValid: false,
   });
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
-    value: "",
-    isValid: null,
-  });
-
-  const { isValid: emailIsValid } = emailState;
-  const { isValid: passwordIsValid } = passwordState;
-
-  useEffect(() => {
-    console.log('EFFECT!');
-
-    setFormIsValid(
-      emailIsValid === true && passwordIsValid === true
-    );
-  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    dispatchEmail({ type: "USER_INPUT", value: event.target.value });
+    dispatchForm({ type: "EMAIL_CHANGE", value: event.target.value });
   };
 
   const passwordChangeHandler = (event) => {
-    dispatchPassword({ type: "USER_INPUT", value: event.target.value });
+    dispatchForm({ type: "PASSWORD_CHANGE", value: event.target.value });
   };
 
   const validateEmailHandler = () => {
-    dispatchEmail({ type: "INPUT_BLUR" });
+    dispatchForm({ type: "EMAIL_BLUR" });
   };
 
   const validatePasswordHandler = () => {
-    dispatchPassword({ type: "INPUT_BLUR" });
+    dispatchForm({ type: "PASSWORD_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    props.onLogin(formState.email, formState.password);
   };
 
   return (
@@ -75,34 +93,38 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
+            formState.emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={emailState.value}
+            value={formState.email}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
+            formState.passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={passwordState.value}
+            value={formState.password}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
         </div>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button
+            type="submit"
+            className={classes.btn}
+            disabled={!formState.formIsValid}
+          >
             Login
           </Button>
         </div>
